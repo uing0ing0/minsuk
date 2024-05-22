bc91201c263be960914783cbc3ba8b6dd3d43848
uing0ing0 <sksmsdmsdud91@gist.ac.kr>
2024-05-23 04:33:14 +0900

    .

--- Frontend/pages/youtube.py
+++ Frontend/pages/youtube.py
@@ -4,10 +4,24 @@ import pages
 from utils.page import PageModel
 from utils.init import init_once
 
+logo_path = "E:\eun\Frontend\logo.jpg.png"
 
 if __name__ == '__main__':
     # Init
     settings = init_once()
+    with st.sidebar:
+        st.markdown(
+            """
+            <style>
+            [data-testid="stSidebar"] {
+                background-color: #ed1b1a;  /* Change this to your desired color */
+
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+    st.image(logo_path, width=80)
 
     # Configure metadata
     name = os.path.basename(__file__)[:-3]
